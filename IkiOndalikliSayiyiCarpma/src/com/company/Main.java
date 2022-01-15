8600925f6a16c2441f99f43be42035cb9dcc7494
gizemdemirtaass <79507052+gizemdemirtaass@users.noreply.github.com>
2022-01-15 15:39:24 +0300

    Add files via upload

---
+++ IkiOndalikliSayiyiCarpma/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+     public static void main (String[] args){
+         float s1 = 1.5f;
+         float s2 = 2.0f;
+         float carpim = s1*s2;
+         System.out.println("İki ondalıklı sayının çarpımı: "+carpim);
+
+    }
+}
